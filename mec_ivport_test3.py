d067fe0065675e96f3644f6917d1d9dd82b786c3
jdaniellhebert <jdh@alumni.cmu.edu>
2017-01-04 14:17:22 -0800

    Update mec_ivport_test3.py

--- mec_ivport_test3.py
+++ mec_ivport_test3.py
@@ -9,10 +9,18 @@ iviic.write_control_register((0x01))                # default
 gp.setwarnings(False)
 gp.setmode(gp.BOARD)
 
-gp.setup(7, gp.OUT)
-gp.setup(11, gp.OUT)
-gp.setup(12, gp.OUT)
+gp.setup(7, gp.OUT)     # Selection for all Jumpers
+gp.setup(11, gp.OUT)    # Jumper A -- Enable 1
+gp.setup(12, gp.OUT)    # Jumper A -- Enable 2 
+gp.setup(15, gp.OUT)    # Jumper B -- Enable 1
+gp.setup(16, gp.OUT)    # Jumper B -- Enable 2
 
+# Turn off Cameras on board B
+gp.output(7, False)
+gp.output(15, False)
+gp.output(16, False)
+
+# Turn on Camera 1 on Jumper A
 gp.output(7, False)
 gp.output(11, False)
 gp.output(12, True)
@@ -23,24 +31,64 @@ while c != 'q':
     c = raw_input("Enter Selection (q for quit):")
     if c == '1':
         iviic.write_control_register((0x01))
+        # Turn off Cameras on board B
+        gp.output(7, False)
+        gp.output(15, False)
+        gp.output(16, False)
+        # Turn on Camera 1 on board A
         gp.output(7, False)
         gp.output(11, False)
         gp.output(12, True)
     elif c == '2':
         iviic.write_control_register((0x02))
+        # Turn off Cameras on board B
+        gp.output(7, False)
+        gp.output(15, False)
+        gp.output(16, False)
+        # Turn on Camera 2 on board A
         gp.output(7, True)
         gp.output(11, False)
         gp.output(12, True)
     elif c == '3':
         iviic.write_control_register((0x04))
+        # Turn off Cameras on board B
+        gp.output(7, False)
+        gp.output(15, False)
+        gp.output(16, False)
+         # Turn on Camera 3 on board A
         gp.output(7, False)
         gp.output(11, True)
         gp.output(12, False)
     elif c == '4':
         iviic.write_control_register((0x08))
+        # Turn off Cameras on board B
+        gp.output(7, False)
+        gp.output(15, False)
+        gp.output(16, False)
+         # Turn on Camera 4 on board A
         gp.output(7, True)
         gp.output(11, True)
         gp.output(12, False)
+    elif c == '5':
+        iviic.write_control_register((0x01))
+        # Turn off Cameras on board A
+        gp.output(7, False)
+        gp.output(11, False)
+        gp.output(12, False)
+        # Turn on Camera 1 on board A
+        gp.output(7, False)
+        gp.output(15, False)
+        gp.output(16, True)
+    elif c == '6':
+        iviic.write_control_register((0x01))
+        # Turn off Cameras on board A
+        gp.output(7, False)
+        gp.output(11, False)
+        gp.output(12, False)
+        # Turn on Camera 1 on board A
+        gp.output(7, False)
+        gp.output(15, False)
+        gp.output(16, True)
     else:
         continue
 
