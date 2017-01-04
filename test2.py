1ae9a60d7e7910e63a456269b934f75958285f0a
jdaniellhebert <jdh@alumni.cmu.edu>
2017-01-04 03:04:07 -0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -25,8 +25,11 @@
 #notes           :A indicates that Ivport jumper setting
 #python_version  :2.7
 #==============================================================================
-
+import IIC
 import RPi.GPIO as gp
+
+iviic = IIC.IIC(addr=(0x70), bus_enable =(0x01))
+iviic.write_control_register((0x01))                # default
 
 gp.setwarnings(False)
 gp.setmode(gp.BOARD)
@@ -44,18 +47,22 @@ c = ''
 while c != 'q':
     c = raw_input("Enter Selection (q for quit):")
     if c == '1':
+        iviic.write_control_register((0x01))
         gp.output(7, False)
         gp.output(11, False)
         gp.output(12, True)
     elif c == '2':
+        iviic.write_control_register((0x02))
         gp.output(7, True)
         gp.output(11, False)
         gp.output(12, True)
     elif c == '3':
+        iviic.write_control_register((0x04))
         gp.output(7, False)
         gp.output(11, True)
         gp.output(12, False)
     elif c == '4':
+        iviic.write_control_register((0x08))
         gp.output(7, True)
         gp.output(11, True)
         gp.output(12, False)
